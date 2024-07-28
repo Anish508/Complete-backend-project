9c25e08decd4dc5e062fc5e586ba5a8821ea2fb1
“Anish508” <anishbarke9741@gmail.com>
2024-07-28 21:08:37 +0530

    add video and user model

---
+++ Advance backend/src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+
+    avatar: {
+      type: String, //cloudinary service (url)
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    whatchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshTokens: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next;
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAcessTokens = function(){
+  return jwt.sign(
+    {
+      _id:this.id,
+      email:this.email,
+      username:this.username,
+      fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshTokens = function(){
+  return jwt.sign(
+    {
+      _id:this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REPRESH_TOKEN_EXPIRY
+    }
+  )
+}
+export const User = mongoose.model("User", userSchema);
